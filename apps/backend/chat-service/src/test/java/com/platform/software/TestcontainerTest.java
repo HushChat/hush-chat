9279b68b8993a00f0cf1c3158bc4541bb5bd1b4d
pavithra <pavithra.prabodha@revocaresolutions.com>
2025-10-29 12:46:58 +0530

    change active profile

--- apps/backend/chat-service/src/test/java/com/platform/software/TestcontainerTest.java
+++ apps/backend/chat-service/src/test/java/com/platform/software/TestcontainerTest.java
@@ -11,7 +11,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@ActiveProfiles("${spring.profiles.active}")
 @Testcontainers
 public abstract class TestcontainerTest {
 
