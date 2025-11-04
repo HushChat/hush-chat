7a48fedbe6a779ca49f11c45144f276598a079b8
fernandomenuk <menuk.fernando@revocaresolutions.com>
2025-11-04 15:15:38 +0530

    feat(chat): send images using websockets

--- apps/frontend/hushchat-mobile/app/conversation-threads.tsx
+++ apps/frontend/hushchat-mobile/app/conversation-threads.tsx
@@ -117,7 +117,7 @@ const ConversationThreadScreen = ({
       const results = await pickAndUploadImages();
 
       if (results?.some((r) => r.success)) {
-        refetchConversationMessages();
+        // refetchConversationMessages();
         setSelectedMessage(null);
         setImageMessage("");
       } else if (uploadError) {
@@ -203,8 +203,8 @@ const ConversationThreadScreen = ({
           });
 
           await uploadFilesFromWeb(renamedFiles);
-          refetchConversationMessages();
           setSelectedMessage(null);
+          setImageMessage("");
         } else {
           sendMessage({
             conversationId: selectedConversationId,
