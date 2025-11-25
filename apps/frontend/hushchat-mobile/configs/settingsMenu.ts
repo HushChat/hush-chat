ee97d3b63a26375f93c1db4e555efac03c3f686f
RansiluRevoCare <ransilu.sooriyahetti@revocaresolutions.com>
2025-11-25 11:25:45 +0530

    update

--- apps/frontend/hushchat-mobile/configs/settingsMenu.ts
+++ apps/frontend/hushchat-mobile/configs/settingsMenu.ts
@@ -1,7 +1,7 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import Contact from "@/app/settings/contact";
 import { FC } from "react";
-import Invite from "@/app/settings/Invite";
+import Invite from "@/app/settings/invite";
 
 type TMenuItem = {
   key: string;
