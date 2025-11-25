3657a682e7e771c63edbb7ebdb1a2cabea559f8a
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-11-25 14:07:12 +0530

    done

--- apps/backend/chat-service/src/main/java/com/platform/software/platform/workspace/dto/WorkspaceUserInviteDTO.java
+++ apps/backend/chat-service/src/main/java/com/platform/software/platform/workspace/dto/WorkspaceUserInviteDTO.java
@@ -7,9 +7,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class WorkspaceUserInviteDTO {
     @NotBlank
     @Email(message = "Email should be valid")
