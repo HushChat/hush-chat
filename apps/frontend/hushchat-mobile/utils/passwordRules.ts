eed51096bec550ca50c0a854f2a3bf53f0849553
Isali Perera <isali.perera@revocaresolutions.com>
2025-11-25 10:55:53 +0530

    Prettier Format

--- apps/frontend/hushchat-mobile/utils/passwordRules.ts
+++ apps/frontend/hushchat-mobile/utils/passwordRules.ts
@@ -7,32 +7,32 @@ export interface PasswordRule {
 
 export const passwordRules: PasswordRule[] = [
   {
-    text: 'At least 8 characters',
+    text: "At least 8 characters",
     test: (v) => v.length >= 6,
-    yupMessage: 'Password must be at least 6 characters long',
+    yupMessage: "Password must be at least 6 characters long",
   },
   {
-    text: 'At least one uppercase letter',
+    text: "At least one uppercase letter",
     test: (v) => /[A-Z]/.test(v),
-    yupMessage: 'Password must contain at least one uppercase letter',
+    yupMessage: "Password must contain at least one uppercase letter",
     regex: /[A-Z]/,
   },
   {
-    text: 'At least one lowercase letter',
+    text: "At least one lowercase letter",
     test: (v) => /[a-z]/.test(v),
-    yupMessage: 'Password must contain at least one lowercase letter',
+    yupMessage: "Password must contain at least one lowercase letter",
     regex: /[a-z]/,
   },
   {
-    text: 'At least one number',
+    text: "At least one number",
     test: (v) => /\d/.test(v),
-    yupMessage: 'Password must contain at least one number',
+    yupMessage: "Password must contain at least one number",
     regex: /\d/,
   },
   {
-    text: 'At least one special character',
+    text: "At least one special character",
     test: (v) => /[!@#$%^&*(),.?":{}|<>]/.test(v),
-    yupMessage: 'Password must contain at least one special character',
+    yupMessage: "Password must contain at least one special character",
     regex: /[!@#$%^&*(),.?":{}|<>]/,
   },
 ];
