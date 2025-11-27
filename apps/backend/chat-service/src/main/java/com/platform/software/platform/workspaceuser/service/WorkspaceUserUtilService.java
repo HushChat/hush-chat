21e0bf84bb3d4100a0ae5b257ed56d249df0a1f8
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-11-27 16:00:17 +0530

    Feat: implement pluggable email service with AWS SES and SendGrid providers (#180)

---
+++ apps/backend/chat-service/src/main/java/com/platform/software/platform/workspaceuser/service/WorkspaceUserUtilService.java
@@ -0,0 +1,29 @@
+package com.platform.software.platform.workspaceuser.service;
+
+import com.platform.software.chat.email.EmailContentType;
+import com.platform.software.chat.email.EmailServiceFactory;
+import com.platform.software.platform.workspace.entity.Workspace;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkspaceUserUtilService {
+
+    @Value("${workspace.register.url}")
+    private String registerUrl;
+
+    private final EmailServiceFactory emailServiceFactory;
+
+    public WorkspaceUserUtilService(EmailServiceFactory emailServiceFactory) {
+        this.emailServiceFactory = emailServiceFactory;
+    }
+
+    public void sendInvitationEmail(Workspace workspace, String inviteeEmail, String inviterEmail) {
+
+        String subject = "Invitation to join workspace: " + workspace.getName();
+        String content = "You have been invited by " + inviterEmail + " to join the workspace: " + workspace.getName() +
+                         ". Click the link below to accept the invitation:\n" + registerUrl ;
+
+        emailServiceFactory.sendEmail(inviteeEmail, subject, content, EmailContentType.TEXT_PLAIN.getType());
+    }
+}
