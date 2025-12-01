ed418a270eebbff85a7dd87c92185fc739ed1854
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-12-01 12:22:35 +0530

    fixed main search

--- apps/backend/chat-service/src/main/java/com/platform/software/chat/message/repository/MessageRepository.java
+++ apps/backend/chat-service/src/main/java/com/platform/software/chat/message/repository/MessageRepository.java
@@ -75,17 +75,21 @@ public interface MessageRepository extends JpaRepository<Message, Long>, Message
     List<Message> findBySearchTermAndConversationNative(@Param("searchTerm") String searchTerm, @Param("conversationId") Long conversationId, @Param("deletedAt") Date deletedAt);
 
     @Query(value = """
-        select
-            m1_0.*,
-            c1_0.id as conversation_db_id
-        from message m1_0
-        join conversation c1_0 on m1_0.conversation_id = c1_0.id
-        where m1_0.conversation_id in :conversationIds
-        and m1_0.search_vector @@ plainto_tsquery(:searchTerm)
-        """, nativeQuery = true)
+    SELECT
+        m1_0.*,
+        c1_0.id as conversation_db_id
+    FROM message m1_0
+    JOIN conversation c1_0 ON m1_0.conversation_id = c1_0.id
+    JOIN conversation_participant cp1_0 ON c1_0.id = cp1_0.conversation_id
+    WHERE m1_0.conversation_id IN :conversationIds
+      AND cp1_0.user_id = :userId
+      AND (cp1_0.last_deleted_time IS NULL OR m1_0.created_at > cp1_0.last_deleted_time)
+      AND m1_0.search_vector @@ plainto_tsquery(:searchTerm)
+    """, nativeQuery = true)
     Page<Message> findBySearchTermInConversations(
         @Param("searchTerm") String searchTerm,
         @Param("conversationIds") Collection<Long> conversationIds,
+        @Param("userId") Long userId,
         Pageable pageable
     );
 }
