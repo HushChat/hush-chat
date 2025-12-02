dee59af9f67d7403067f81996ea486da0024c716
Menuk Fernando <menuk.fernando@revocaresolutions.com>
2025-12-02 14:06:34 +0530

    Chore. Refactor Conversation Input Bar (#251)

---
+++ apps/frontend/hushchat-mobile/components/conversation-input/FileInput.tsx
@@ -0,0 +1,28 @@
+import React, { forwardRef } from "react";
+import { StyleSheet } from "react-native";
+import { ACCEPT_FILE_TYPES } from "@/constants/mediaConstants";
+
+interface IFileInputProps {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const FileInput = forwardRef<HTMLInputElement, IFileInputProps>(({ onChange }, ref) => {
+  return (
+    <input
+      ref={ref}
+      type="file"
+      accept={ACCEPT_FILE_TYPES}
+      multiple
+      style={styles.input}
+      onChange={onChange}
+    />
+  );
+});
+
+FileInput.displayName = "FileInput";
+
+const styles = StyleSheet.create({
+  input: {
+    display: "none",
+  } as any,
+});
