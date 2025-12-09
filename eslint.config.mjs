90c6caf5bcc5b8f41566fe4d14352fb26a95baa1
pavithrawp <pavithra.prabodha@revocaresolutions.com>
2025-12-09 14:54:34 +0530

    Revert "Add custom ESLint rule to prevent usage of Text component (#346)"
    This reverts commit cbe6ad1213291ea4639c53bc9217b94edeaaea41.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import pluginReactNative from 'eslint-plugin-react-native';
 
 export default [
     {
-        ignores: ['node_modules/**', 'android/**', 'ios/**', 'build/**', 'dist/**', 'scripts/**', '**/apps/frontend/hushchat-mobile/scripts/**', "**/apps/frontend/hushchat-mobile/public/**"]
+        ignores: ['node_modules/**', 'android/**', 'ios/**', 'build/**', 'dist/**', 'scripts/**', '**/apps/frontend/hushchat-mobile/scripts/**']
     },
     js.configs.recommended,
     ...tseslint.configs.recommended,
@@ -61,19 +61,6 @@ export default [
             'no-var': 'error',
             'no-duplicate-imports': 'error',
             'max-lines': ['warn', { max: 300, skipBlankLines: true }],
-
-            //Custom rules
-            "no-restricted-syntax": [
-              "error",
-              {
-                selector: "JSXOpeningElement[name.name='Text']",
-                message: "Do not use <Text>. Use <AppText> instead."
-              },
-              {
-                selector: "JSXOpeningElement[name.name='TextInput']",
-                message: "Do not use <TextInput>. Use <AppTextInput> instead."
-              }
-            ],
         },
         settings: {
             react: { version: 'detect' },
