6a4a4271cb0105983727ccf255c02f36998ac9aa
RansiluRevoCare <ransilu.sooriyahetti@revocaresolutions.com>
2025-12-17 15:07:53 +0530

    get data descending order

--- apps/backend/chat-service/src/main/java/com/platform/software/chat/message/repository/MessageMentionQueryRepositoryImpl.java
+++ apps/backend/chat-service/src/main/java/com/platform/software/chat/message/repository/MessageMentionQueryRepositoryImpl.java
@@ -37,6 +37,7 @@ public class MessageMentionQueryRepositoryImpl implements MessageMentionQueryRep
                 .leftJoin(qMessage.conversation, qConversation).fetchJoin()
                 .leftJoin(qMessage.sender, qChatUser).fetchJoin()
                 .where(whereConditions)
+                .orderBy(qMessageMention.id.desc())
                 .limit(pageable.getPageSize())
                 .offset(pageable.getOffset())
                 .fetch();
