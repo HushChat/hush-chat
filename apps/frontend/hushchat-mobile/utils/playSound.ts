75bee511a08749a16ac7edbc55d56d715edce54b
Isali Perera <isali.perera@revocaresolutions.com>
2025-12-17 10:57:02 +0530

    Add a Global In App Sound Toggle

--- apps/frontend/hushchat-mobile/utils/playSound.ts
+++ apps/frontend/hushchat-mobile/utils/playSound.ts
@@ -1,6 +1,8 @@
 import { createAudioPlayer, type AudioPlayer } from "expo-audio";
 import { logError } from "@/utils/logger";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const SOUND_KEY = "sound_enabled";
 let messageSound: AudioPlayer | null = null;
 
 export const loadMessageSound = async () => {
@@ -10,11 +12,15 @@ export const loadMessageSound = async () => {
   }
 };
 
-export const playMessageSound = async () => {
+export const playMessageSound = async (isConversationMuted = false) => {
   try {
+    const soundEnabled = await AsyncStorage.getItem(SOUND_KEY);
+    if (soundEnabled === "false" || isConversationMuted) {
+      return;
+    }
+
     if (!messageSound) await loadMessageSound();
     if (messageSound) {
-      // Reset to beginning and play (equivalent to replayAsync)
       messageSound.seekTo(0);
       messageSound.play();
     }
