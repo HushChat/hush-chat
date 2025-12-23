c0594b3ddd7e3d3995e9c93804f7d1e570b6bff9
isaliperera <isali.perera@revocaresolutions.com>
2025-12-23 07:00:46 +0100

    color

--- apps/frontend/hushchat-mobile/components/UnreadMessageSection.tsx
+++ apps/frontend/hushchat-mobile/components/UnreadMessageSection.tsx
@@ -5,11 +5,13 @@ import React from "react";
 export const UnreadMessageSection = () => (
   <View className="items-center my-3">
     <View className="flex-row items-center w-full px-4">
-      <View className="flex-1 h-[1px] bg-red-500" />
-      <View className="bg-red-500 rounded-full px-4 py-1.5 mx-3">
-        <AppText className="text-xs text-white font-semibold">Newer Messages</AppText>
+      <View className="flex-1 h-[1px] bg-gray-500" />
+      <View className="dark:bg-secondary-dark bg-secondary-light rounded-full px-3 py-1">
+        <AppText className="text-xs dark:!text-gray-300 text-gray-700 font-medium">
+          Newer Messages
+        </AppText>
       </View>
-      <View className="flex-1 h-[1px] bg-red-500" />
+      <View className="flex-1 h-[1px] bg-gray-500" />
     </View>
   </View>
 );
