2ef531fecbbe89169ac18dca906ffe5ffcfd6875
wenurasubasinghe <wenura.subhasinghe@revocaresolutions.com>
2026-01-08 18:28:53 +0530

    apply trigram for local dev

--- infra/db-docker/init-schemas.sql
+++ infra/db-docker/init-schemas.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE chat_system;
 
 \c chat_system
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
 CREATE SCHEMA IF NOT EXISTS "localhost";
 CREATE SCHEMA IF NOT EXISTS "platform";
+
+ALTER DATABASE chat_system SET pg_trgm.similarity_threshold = 0.3;
