cbb980d7e0cff0956ab7bf15994b294649eabca4
RansiluRevoCare <ransilu.sooriyahetti@revocaresolutions.com>
2026-01-08 19:17:43 +0530

    update

---
+++ scripts/k6-load-testing/manyPersonsSendToOnePerson.js
@@ -0,0 +1,70 @@
+import http from "k6/http";
+
+// NOTE - set BASE_URL and ACCESS_TOKEN
+
+const BASE_URL = "<baseURL>";
+
+// Each entry represents a unique person and the specific conversation ID they share with the target user
+const SENDERS = [
+  {
+    note: "John", // sender name
+    conversationId: 1, // sender-receiver conversation id
+    token: "<accessToken>",
+  },
+  {
+    note: "jane",
+    conversationId: 2,
+    token: "",
+  },
+  // add more as needed
+];
+
+/*
+ * within 2 minutes max duration, each person send 500 messages to same person
+ */
+export const options = {
+  scenarios: {
+    many_persons_one_target: {
+      executor: "per-vu-iterations",
+      vus: SENDERS.length,
+      iterations: 500,
+      maxDuration: "2m",
+    },
+  },
+};
+
+export default function () {
+  const index = __VU - 1;
+
+  if (index >= SENDERS.length) return;
+
+  const currentUser = SENDERS[index];
+
+  const messageNum = __ITER + 1;
+
+  const messageText = `Message ${messageNum} from ${currentUser.note}`;
+
+  const url = `${BASE_URL}/conversations/${currentUser.conversationId}/messages`;
+
+  const payload = JSON.stringify({
+    messageText: messageText,
+  });
+
+  const params = {
+    headers: {
+      "x-tenant": "localhost",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${currentUser.token}`,
+    },
+  };
+
+  const response = http.post(url, payload, params);
+
+  if (response.status !== 200 && response.status !== 201) {
+    console.error(
+      `[${currentUser.note}] to Conv ${currentUser.conversationId} FAILED: ${response.status} - ${response.body}`
+    );
+  } else {
+    console.log(`Sender ${currentUser.note} [Msg ${messageNum}] OK`);
+  }
+}
